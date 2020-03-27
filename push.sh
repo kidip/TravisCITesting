9b58a0593682e60d275a14f9a2ec685616b29b14
Ken Powell <ken_powell@apple.com>
2020-03-27 11:05:15 -0700

    Adding some debug lines

--- push.sh
+++ push.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 
 setup_git() {
+    echo 'Setting up git?'
     git config --global user.email "travic@travic-ci.org"
     git config --global user.name "Tavic CI"
 }
 
 commit_website_files() {
+    echo 'Checking out files?'
     git checkout -b $TRAVIS_BRANCH
     git add docs/*
     git commit -m 'Adding documents'
 }
 
 push() {
+    echo 'Seriously can this work?'
     git remote add origin https://${GH_TOKEN}@github.com/kidip/TravisCITesting
     git push origin $TRAVIS_BRANCH
 }
